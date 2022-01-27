19f85e25ab3f91a47384e8fe94c2e92d49f27151
j.dawn <52862474+JacquelineDawn@users.noreply.github.com>
2022-01-27 14:52:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,38 @@ const scrollFunk = () => {
 }
 
 window.addEventListener('scroll', scrollFunk);
+
+
+
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () =>  {
+  
+ const visibility = primaryNav.getAttribute('data-visible'); 
+  
+ console.log(visibility); 
+ 
+if(visibility === 'false'){
+  primaryNav.setAttribute('data-visible', true);
+  navToggle.setAttribute('aria-expanded', true);
+} else if (visibility === "true") {
+  primaryNav.setAttribute('data-visible', false);
+   navToggle.setAttribute('aria-expanded', false);
+}
+  
+  
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
